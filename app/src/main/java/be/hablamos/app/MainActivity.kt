3e30dcc755b8a56c2862fe178bce500d56976eef
Jhamende <jonathanhamende@gmail.com>
2026-07-31 12:58:00 +0200

    Add Android entry point

---
+++ app/src/main/java/be/hablamos/app/MainActivity.kt
@@ -0,0 +1,23 @@
+package be.hablamos.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import be.hablamos.app.ui.HablamosApp
+import be.hablamos.app.ui.theme.HablamosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HablamosTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    HablamosApp()
+                }
+            }
+        }
+    }
+}
